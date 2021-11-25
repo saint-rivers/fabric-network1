913b2807f8b1f33c4a3a78e2ecf35b1dc9ae7b2e
dayan <eam.daayn@gmail.com>
2021-11-25 19:10:18 +0700

    update start script

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@
 #       EXPORT ENVIRONMENT VARIABLES
 ########################################################################
 
-
-
+export FABRIC_CFG_PATH=$PWD/config
+export PATH=$PATH:$PWD/bin
 
 ########################################################################
 #       GENERATE CRYPTO MATERIALS
@@ -48,10 +48,12 @@ docker exec -it cli bash -c ". scripts/joinPeersToChannelPeer2.sh"
 #       INSTALLING AND APPROVING CHAINCODE
 ########################################################################
 
-
+docker exec -it cli bash -c ". scripts/installChaincodeOnPeer.sh"
+docker exec -it cli bash -c ". scripts/installChaincodeOnPeer2.sh"
 
 
 ########################################################################
 #       COMMITING CHAINCODE
 ########################################################################
 
+docker exec -it cli bash -c ". scripts/commitChaincode.sh"
